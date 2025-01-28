1589d4ef1ca102b04e40146cd26297cf189b18ac
Zachary Baldwin <zacharyjbaldwin@gmail.com>
2025-01-27 21:05:34 -0600

    Initial commit

---
+++ QuickDraw/Hubs/GameHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+
+namespace QuickDraw.Hubs
+{
+    public class GameHub : Hub
+    {
+        private static readonly ConcurrentDictionary<string, string> _connectedUsers = new();
+        private const string UPDATE_USER_LIST = "UpdateUserList";
+        private const string UPDATE_SCORE = "UpdateScore";
+
+        public async Task JoinGame(string displayName)
+        {
+            if (!string.IsNullOrEmpty(displayName))
+            {
+                _connectedUsers.TryAdd(Context.ConnectionId, displayName);
+                await UpdateUserList();
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (_connectedUsers.TryRemove(Context.ConnectionId, out _))
+            {
+                await UpdateUserList();
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        private async Task UpdateUserList()
+        {
+            await Clients.All.SendAsync(UPDATE_USER_LIST, _connectedUsers.Values);
+        }
+    }
+}
---
+++ QuickDraw/Program.cs
@@ -0,0 +1,41 @@
+using QuickDraw.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseCors(options =>
+{
+    options
+        .AllowAnyHeader()
+        .AllowCredentials()
+        .AllowAnyMethod()
+        .WithOrigins("https://localhost:44495/");
+});
+
+app.MapHub<GameHub>("/gamehub");
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
